a95cc17a9b53cfa0ca7469b31f57bb997947630f
Vaibhav <vaibhavasrhney999@gmail.com>
2024-06-06 15:18:19 +0530

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -13,4 +14,17 @@ mongoose
 const app = express();
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
+});
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
