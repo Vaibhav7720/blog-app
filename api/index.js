a51930c131b2d0379fc4f1cd129e8c052edb3d27
Vaibhav <vaibhavasrhney999@gmail.com>
2024-06-07 15:40:47 +0530

    Put post functionality

--- api/index.js
+++ api/index.js
@@ -2,7 +2,14 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
+import cookieParser from "cookie-parser";
+import path from "path";
+
 dotenv.config();
+
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -11,10 +18,25 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+const __dirname = path.resolve();
+
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
+app.use("/api/comment", commentRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
